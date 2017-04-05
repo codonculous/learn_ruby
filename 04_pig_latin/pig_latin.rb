a93bfcd561b3ba1889a18e5bc5417ebece8a9331
codonculous <feidameng01@gmail.com>
2017-04-05 00:53:53 -0400

    0

---
+++ 04_pig_latin/pig_latin.rb
@@ -0,0 +1,27 @@
+
+def translate(str)
+  array = str.split
+  puts array
+  vowel = ["a","e","i","o"]
+  new_array =[]
+
+  array.each do |a|
+
+    if vowel.include?(a[0]) == false
+      v_index = []
+      puts "a: #{a}"
+      vowel.each do |v|
+        v_index.push(a.index(v))
+      end
+      sliced = a.slice!(0,v_index.compact.min)
+      puts "sliced: #{sliced}"
+      puts "a:#{a}"
+      a = "#{a}#{sliced}"
+    end
+    new_array.push("#{a}ay")
+  end
+  puts new_array
+  new_array.join(" ")
+end
+
+translate("quiet")
